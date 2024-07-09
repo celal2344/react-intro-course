0b0749a0d339316571bcb66e5f2273dbdb5d1938
Celal Özdemir <celal2344@gmail.com>
2024-07-09 21:35:55 +0300

    finished currency converter app using a free currency api, axios, and usestates

--- 6-api/src/App.jsx
+++ 6-api/src/App.jsx
@@ -5,31 +5,45 @@ import { useEffect } from 'react'
 const BASE_URL = 'http://localhost:3005'
 
 function App() {
-  const getAllUsers = async () => {
-    const response = await axios.get(BASE_URL + '/users')
-    console.log(response)
+  // const getAllUsers = async () => {
+  //   const response = await axios.get(BASE_URL + '/users')
+  //   console.log(response)
+  // }
+  // const getUserById = async (id) => {
+  //   const response = await axios.get(BASE_URL + '/users/' + id)
+  //   console.log(response)
+  // }
+  // const createUser = async (newUser) => {
+  //   const response = await axios.post(BASE_URL + '/users', newUser)
+  //   console.log(response)
+  // }
+  // const updateUser = async (id, updatedUser) => {
+  //   const response = await axios.put(BASE_URL + '/users/' + id, updatedUser)
+  //   console.log(response)
+  // }
+  // const deleteUser = async (id) => {
+  //   const response = await axios.delete(BASE_URL + '/users/' + id)
+  //   console.log(response)
+  // }
+
+  const getUserById = async (userId) => {
+    const response = await axios.get(BASE_URL + '/users/' + userId);
+    return response.data.postId;
   }
-  const getUserById = async (id) => {
-    const response = await axios.get(BASE_URL + '/users/' + id)
-    console.log(response)
+  const getPostById = async (postId) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    return response.data;
   }
-  const createUser = async (newUser) => {
-    const response = await axios.post(BASE_URL + '/users', newUser)
-    console.log(response)
+  const getPost = async () => {
+    const postId = await getUserById(1);
+    console.log(postId)
+    const post = await getPostById(postId);
+    console.log(post)
   }
-  const updateUser = async (id, updatedUser) => {
-    const response = await axios.put(BASE_URL + '/users/' + id, updatedUser)
-    console.log(response)
-  }
-  const deleteUser = async (id) => {
-    const response = await axios.delete(BASE_URL + '/users/' + id)
-    console.log(response)
-  }
+
+
   useEffect(() => {
-    createUser({
-      "name": "celal",
-      "password": "123456",
-    });
+    getPost()
   }, [])
 
   return (
---
+++ currency-app/src/components/Currency.jsx
@@ -0,0 +1,60 @@
+import "../css/Currency.css"
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import axios from "axios"
+import { useEffect, useState } from 'react'
+
+const BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+const API_KEY = "fca_live_QRwsCBTgKGj7UMKeKZz3H8UWIfwLeyFqSn4iP7Qf"
+
+function Currency() {
+    const [amount, setAmount] = useState(10);
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState('TRY');
+    const [result, setResult] = useState(0);
+    //https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_QRwsCBTgKGj7UMKeKZz3H8UWIfwLeyFqSn4iP7Qf&base_currency=TRY
+    const getCurrency = async () => {
+        var currencies = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`)
+        var fromCurrencyAmount = currencies.data.data[toCurrency] * amount
+        setResult(fromCurrencyAmount.toFixed(2))
+    }
+    return (
+        <div className='currency-div'>
+            <div className="header-container">
+                <h2 >CURRENCY APP</h2>
+            </div>
+            <div className="input-container">
+                <input
+                    onChange={(e) => setAmount(e.target.value)}
+                    value={amount}
+                    type='number' className='amount' />
+                <select
+                    onChange={(e) => setFromCurrency(e.target.value)}
+                    className='from-currency-option'>
+                    <option value='USD'>USD</option>
+                    <option value='EUR'>EUR</option>
+                    <option value='TRY'>TL </option>
+                </select>
+                <FaRegArrowAltCircleRight style={
+                    {
+                        fontSize: '24px',
+                        color: 'black',
+                        marginRight: "10px",
+                    }
+                } />
+                <select
+                    onChange={(e) => setToCurrency(e.target.value)}
+                    className='to-currency-option'>
+                    <option value='TRY'>TL </option>
+                    <option value='USD'>USD</option>
+                    <option value='EUR'>EUR</option>
+                </select>
+                <input type='number' className='result' value={result} />
+            </div>
+            <div>
+                <button onClick={getCurrency} className="convert-button">CONVERT</button>
+            </div>
+        </div >
+    )
+}
+
+export default Currency
